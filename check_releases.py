5e48fdcaf3f6843435bd522519d7ec902fc13e26
slush0 <slush@satoshilabs.com>
2016-05-18 22:25:09 +0200

    Updated for python3 due to OpenSSL issues in python2.7 w/ Amazon S3 servers.

--- check_releases.py
+++ check_releases.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import json
-import urllib2
+import requests
 import binascii
 
 def check_plugin():
@@ -25,28 +25,28 @@ def check_plugin():
             continue
 
         latest = v
-        print "Checking files for version", v
-        print "---------------------------------"
+        print("Checking files for version", v)
+        print("---------------------------------")
 
         for p in patterns:
             expected = p % {'version': v}
             if not os.path.isfile(os.path.join('plugin', v, expected)):
                 ok = False
-                print "Missing file %s/%s" % (v, expected)
+                print("Missing file %s/%s" % (v, expected))
 
-    print "Checking latest.txt"
-    print "-------------------"
+    print("Checking latest.txt")
+    print("-------------------")
 
     latest_file = open('plugin/latest.txt', 'r').read().strip()
     if latest != latest_file:
-        print "Expected latest.txt: %s, got %s" % (latest, latest_file)
+        print("Expected latest.txt: %s, got %s" % (latest, latest_file))
         ok = False
 
     return ok
 
 def check_firmware():
-    print "Checking firmware availability"
-    print "------------------------------"
+    print("Checking firmware availability")
+    print("------------------------------")
 
     ok = True
     releases = json.loads(open('firmware/releases.json', 'r').read())
@@ -55,33 +55,33 @@ def check_firmware():
         version = '.'.join([ str(x) for x in r['version'] ])
 
         if version not in firmware:
-            print "Missing '%s' in '%s'" % (version, firmware)
+            print("Missing '%s' in '%s'" % (version, firmware))
             ok = False
 
-        print "Checking", firmware
+        print("Checking", firmware)
         if firmware.startswith('http'):
-            ret = urllib2.urlopen(firmware)
-            if ret.code != 200:
-                print "Missing firmware file", firmware
+            ret = requests.get(firmware)
+            if ret.status_code != 200:
+                print("Missing firmware file", firmware)
                 ok = False
                 data = ''
             else:
-                data = ret.read()
+                data = bytes(ret.text, 'utf-8')
 
         else:
             if not os.path.exists(firmware[len('/data/'):]):
-                print "Missing firmware file", firmware
+                print("Missing firmware file", firmware)
                 ok = False
                 data = ''
             else:
                 data = open(firmware[len('/data/'):], 'r').read()
 
-        if not data.startswith(binascii.hexlify('TRZR')):
-            print "Corrupted file header:", firmware
+        if not data.startswith(binascii.hexlify(b'TRZR')):
+            print("Corrupted file header:", firmware)
             ok = False
 
         if len(data) / 2 > 512*1024 - 32*1024: # Firmware - header - signatures
-            print "File size is over limit:", firmware
+            print("File size is over limit:", firmware)
             ok = False
 
     return ok
@@ -92,9 +92,9 @@ if __name__ == '__main__':
     ok &= check_firmware()
 
     if ok:
-        print "EVERYTHING IS OK"
+        print("EVERYTHING IS OK")
         exit(0)
     else:
-        print "SOME PROBLEMS FOUND"
+        print("SOME PROBLEMS FOUND")
         exit(1)
 
